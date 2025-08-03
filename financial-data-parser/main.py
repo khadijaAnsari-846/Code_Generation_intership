a27af7aafa0f83332a22bdd6e42163d54f9a6583
khadijaAnsari-846 <ansarikhadija846@gmail.com>
2025-08-03 14:15:44 +0500

    Delete financial-data-parser directory

--- financial-data-parser/main.py
+++
@@ -1,8 +0,0 @@
-
-from src.core.main_design import main
-import warnings
-warnings.filterwarnings("ignore")
-warnings.filterwarnings("ignore", category=UserWarning)  # Suppress the warning
-
-if __name__ == "__main__":
-    main()
